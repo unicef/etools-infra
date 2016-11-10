22e973dbd6383944e4ccb3d44b22d140a285817e
Sumit Chachra <sumit@tivix.com>
2016-11-10 13:29:57 -0500

    Added DJANGO_SETTINGS_MODULE in dockerfile and fixed fabfile ssh.

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,7 @@ from fabric.api import local, env, settings
 
 
 def ssh(service):
-    local('docker exec -it etoolsinfra_%s_1 bash' % service)
+    local('docker exec -it etoolsinfra_%s_1 /bin/sh' % service)
 
 def devup(quick=False):
     local('docker-compose -f docker-compose.dev.yml up %s' % ('--build' if not quick else ''))
