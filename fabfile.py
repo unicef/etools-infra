e054063ab2e3d823f77c87649449567c5321f110
Sumit Chachra <sumit@tivix.com>
2016-11-10 15:06:21 -0500

    Adding --force-recreate to fabfile, removing volume section in dev docker-compose file. pulled in new submodule code

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,7 @@ def ssh(service):
     local('docker exec -it etoolsinfra_%s_1 /bin/sh' % service)
 
 def devup(quick=False):
-    local('docker-compose -f docker-compose.dev.yml up %s' % ('--build' if not quick else ''))
+    local('docker-compose -f docker-compose.dev.yml up --force-recreate %s' % ('--build' if not quick else ''))
 
 def up(quick=False):
-    local('docker-compose -f docker-compose.yml up')
+    local('docker-compose -f docker-compose.yml up --force-recreate')
