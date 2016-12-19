fa515898fc948c04955cbb65ba8966ceb4497277
robertavram <robert.avram@gmail.com>
2016-12-19 13:40:52 -0500

    Add backend migrations command

--- fabfile.py
+++ fabfile.py
@@ -7,5 +7,8 @@ def ssh(service):
 def devup(quick=False):
     local('docker-compose -f docker-compose.dev.yml up --force-recreate %s' % ('--build' if not quick else ''))
 
+def backend_migrations():
+    local('docker exec etoolsinfra_backend_1 python /code/EquiTrack/manage.py migrate_schemas --noinput')
+
 def up(quick=False):
     local('docker-compose -f docker-compose.yml up --force-recreate')
