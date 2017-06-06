0d89ee0b13261500725c821e65862ca322d210c6
robertavram <robert.avram@gmail.com>
2017-06-06 14:04:17 -0400

    Add debug fabric command

--- fabfile.py
+++ fabfile.py
@@ -19,5 +19,20 @@ def devup_debug(quick=False, ip='192.168.240.248', p='51312'):
 def backend_migrations():
     local('docker exec etoolsinfra_backend_1 python /code/EquiTrack/manage.py migrate_schemas --noinput')
 
+def debug(quick=False, DEBUG_PORT='51312'):
+    try:
+        import netifaces as ni
+    except ImportError:
+        print "You must have the 'netifaces' package installed"
+    ni.ifaddresses('en0')
+    ip = ni.ifaddresses('en0')[2][0]['addr']
+    with shell_env(BACKEND_DEBUG="_debug", DEBUG_IP=ip, DEBUG_PORT=DEBUG_PORT):
+        local('docker-compose -f docker-compose.dev.yml up --force-recreate %s' % ('--build' if not quick else ''))
+
+def remove_docker_containers():
+    local('docker stop $(docker ps -q)')
+    local('docker rm $(docker ps -q)')
+
+
 def up(quick=False):
     local('docker-compose -f docker-compose.yml up --force-recreate')
