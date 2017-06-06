36f28e0e0f26d0789ed271ee4ff9c15f933d8a7a
robertavram <robert.avram@gmail.com>
2017-06-06 14:08:29 -0400

    remove old debug command

--- fabfile.py
+++ fabfile.py
@@ -9,15 +9,6 @@ def ssh(service):
 def devup(quick=False):
     local('docker-compose -f docker-compose.dev.yml up --force-recreate %s' % ('--build' if not quick else ''))
 
-def devup_debug(quick=False, ip='192.168.240.248', p='51312'):
-    '''
-        make sure the debugger is running
-        run ipconfig getifaddr en0 to get your IP address
-        fab devup_debug:True,ip=192.168.240.248,p=51312
-    '''
-    with shell_env(BACKEND_DEBUG="_debug", DEBUG_IP=ip, DEBUG_PORT=p):
-        local('docker-compose -f docker-compose.dev.yml up --force-recreate %s' % ('--build' if not quick else ''))
-
 def backend_migrations():
     local('docker exec etoolsinfra_backend_1 python /code/EquiTrack/manage.py migrate_schemas --noinput')
 
@@ -26,6 +17,7 @@ def debug(quick=False, DEBUG_PORT='51312'):
         import netifaces as ni
     except ImportError:
         print "You must have the 'netifaces' package installed"
+        return
     ni.ifaddresses('en0')
     ip = ni.ifaddresses('en0')[2][0]['addr']
     with shell_env(BACKEND_DEBUG="_debug", DEBUG_IP=ip, DEBUG_PORT=DEBUG_PORT):
