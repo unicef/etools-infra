cc02a19e8eea5ac64d7769fef741a4194166d2f7
Cory Zue <czue@dimagi.com>
2017-08-09 15:18:59 -0400

    use a more useful shell

--- fabfile.py
+++ fabfile.py
@@ -12,7 +12,7 @@ APP_SUBMODULE_DIRECTORIES = (
 
 
 def ssh(service):
-    local('docker exec -it etoolsinfra_%s_1 /bin/sh' % service)
+    local('docker exec -it etoolsinfra_%s_1 /bin/bash' % service)
 
 def devup(quick=False):
     local('docker-compose -f docker-compose.dev.yml up --force-recreate %s' % ('--build' if not quick else ''))
