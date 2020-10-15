a9bab093d38930f37858fa8498cf04d3803bb17a
Domenico <dom.dinicola@gmail.com>
2020-10-15 10:49:57 -0500

    fix tests

--- db/load_db_data.sh
+++ db/load_db_data.sh
@@ -40,7 +40,7 @@ GRANT ALL PRIVILEGES ON DATABASE etools TO etoolusr;
 EOSQL
 
 echo "*** UPDATING DATABASE ***"
-bzcat $DB_DUMP_LOCATION | nice pg_restore -U etoolusr -F t -d etools
+# bzcat $DB_DUMP_LOCATION | nice pg_restore -U etoolusr -F t -d etools
 
 echo "*** DATABASE CREATED! ***"
 
