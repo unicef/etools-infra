3fbd2d6c0acc7b439a52b9b7975387bbaaa51cc1
blu <blu@fossa.localdomain>
2021-12-17 14:08:34 +0200

    Reduce DB image size by 2GB with mounting the dump bz2 file instead of copying into the image

--- db/load_db_data.sh
+++ db/load_db_data.sh
@@ -17,9 +17,9 @@ psql template1 -c "CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;"
 psql -c "CREATE ROLE etoolusr WITH superuser login;"
 psql -c "CREATE DATABASE etools;"
 psql -c "GRANT ALL PRIVILEGES ON DATABASE etools TO etoolusr;"
+echo "*** DATABASE CREATED ***"
 
-echo "*** UPDATING DATABASE ***"
+echo "*** RESTORING DATABASE ***"
 #bzcat $DB_DUMP_LOCATION | nice pg_restore --verbose  -U etoolusr -F t -d etools
 bzcat $DB_DUMP_LOCATION | nice pg_restore -U etoolusr -F c -d etools
-
-echo "*** DATABASE CREATED ***"
+echo "*** COMPLETED DATABASE RESTORE***"
