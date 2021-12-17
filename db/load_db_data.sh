9dab31217595aa1bf6b1a95f7d532bcce1d39fbe
blu <blu@fossa.localdomain>
2021-12-17 12:14:31 +0200

    27620: etools-infra: Update DB setup on Dockerfile, docker-compose so that the restore db1.bz2 is successful

--- db/load_db_data.sh
+++ db/load_db_data.sh
@@ -7,11 +7,16 @@ export DB_DUMP_LOCATION=/tmp/psql_data/db1.bz2
 echo "*** CREATING DATABASE ***"
 
 # create default database
+psql template1 -c "CREATE EXTENSION IF NOT EXISTS pgpool_recovery;"
+psql template1 -c "CREATE EXTENSION IF NOT EXISTS pgpool_adm;"
+psql template1 -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+psql template1 -c "CREATE EXTENSION IF NOT EXISTS postgis_topology;"
+psql template1 -c "CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;"
+psql template1 -c "CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;"
 
 psql -c "CREATE ROLE etoolusr WITH superuser login;"
 psql -c "CREATE DATABASE etools;"
 psql -c "GRANT ALL PRIVILEGES ON DATABASE etools TO etoolusr;"
-
 
 echo "*** UPDATING DATABASE ***"
 #bzcat $DB_DUMP_LOCATION | nice pg_restore --verbose  -U etoolusr -F t -d etools
