a67e2eddb8130fce1f095809cc019fcb7ace136e
blu <blu@fossa.localdomain>
2022-03-04 14:34:58 +0200

    DB setup: mount the db dump instead of copying; add pgpool sys dependency

--- db/load_db_data.sh
+++ db/load_db_data.sh
@@ -7,14 +7,19 @@ export DB_DUMP_LOCATION=/tmp/psql_data/db1.bz2
 echo "*** CREATING DATABASE ***"
 
 # create default database
+psql template1 -c "CREATE EXTENSION IF NOT EXISTS pgpool_recovery;"
+psql template1 -c "CREATE EXTENSION IF NOT EXISTS pgpool_adm;"
+psql template1 -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+psql template1 -c "CREATE EXTENSION IF NOT EXISTS postgis_topology;"
+psql template1 -c "CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;"
+psql template1 -c "CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;"
 
 psql -c "CREATE ROLE etoolusr WITH superuser login;"
 psql -c "CREATE DATABASE etools;"
 psql -c "GRANT ALL PRIVILEGES ON DATABASE etools TO etoolusr;"
+echo "*** DATABASE CREATED ***"
 
-
-echo "*** UPDATING DATABASE ***"
+echo "*** RESTORING DATABASE ***"
 #bzcat $DB_DUMP_LOCATION | nice pg_restore --verbose  -U etoolusr -F t -d etools
 bzcat $DB_DUMP_LOCATION | nice pg_restore -U etoolusr -F c -d etools
-
-echo "*** DATABASE CREATED ***"
+echo "*** COMPLETED DATABASE RESTORE***"
