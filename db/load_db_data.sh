1c830fe113fffbe88d150b75bccc963d17278098
blu <blu@fossa.localdomain>
2022-03-04 17:01:40 +0200

    code review: remove postgres extensions

--- db/load_db_data.sh
+++ db/load_db_data.sh
@@ -6,14 +6,6 @@ export DB_DUMP_LOCATION=/tmp/psql_data/db1.bz2
 
 echo "*** CREATING DATABASE ***"
 
-# create default database
-psql template1 -c "CREATE EXTENSION IF NOT EXISTS pgpool_recovery;"
-psql template1 -c "CREATE EXTENSION IF NOT EXISTS pgpool_adm;"
-psql template1 -c "CREATE EXTENSION IF NOT EXISTS postgis;"
-psql template1 -c "CREATE EXTENSION IF NOT EXISTS postgis_topology;"
-psql template1 -c "CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;"
-psql template1 -c "CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;"
-
 psql -c "CREATE ROLE etoolusr WITH superuser login;"
 psql -c "CREATE DATABASE etools;"
 psql -c "GRANT ALL PRIVILEGES ON DATABASE etools TO etoolusr;"
